baa23db0968719751a343e5501d48c34206da45d
dorianj29 <dorianzp95@uniandes.edu.ec>
2024-08-25 23:38:35 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace ProyectoIntegrador
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pnl_titu = new System.Windows.Forms.Panel();
+            this.pnl_imagen = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.txt_UsuarioLogin = new System.Windows.Forms.TextBox();
+            this.txt_ContraseñaLogin = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_Ingresar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnl_titu
+            // 
+            this.pnl_titu.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pnl_titu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnl_titu.Location = new System.Drawing.Point(0, 0);
+            this.pnl_titu.Name = "pnl_titu";
+            this.pnl_titu.Size = new System.Drawing.Size(905, 30);
+            this.pnl_titu.TabIndex = 0;
+            // 
+            // pnl_imagen
+            // 
+            this.pnl_imagen.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pnl_imagen.BackgroundImage")));
+            this.pnl_imagen.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnl_imagen.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pnl_imagen.Location = new System.Drawing.Point(494, 30);
+            this.pnl_imagen.Name = "pnl_imagen";
+            this.pnl_imagen.Size = new System.Drawing.Size(411, 436);
+            this.pnl_imagen.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(93, 54);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(316, 75);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.Image = ((System.Drawing.Image)(resources.GetObject("label2.Image")));
+            this.label2.Location = new System.Drawing.Point(88, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 54);
+            this.label2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(152, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(236, 27);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Para un futuro mejor";
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_exit.BackgroundImage")));
+            this.btn_exit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_exit.Location = new System.Drawing.Point(0, 36);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(57, 42);
+            this.btn_exit.TabIndex = 6;
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // txt_UsuarioLogin
+            // 
+            this.txt_UsuarioLogin.Location = new System.Drawing.Point(67, 268);
+            this.txt_UsuarioLogin.Name = "txt_UsuarioLogin";
+            this.txt_UsuarioLogin.Size = new System.Drawing.Size(373, 22);
+            this.txt_UsuarioLogin.TabIndex = 7;
+            // 
+            // txt_ContraseñaLogin
+            // 
+            this.txt_ContraseñaLogin.Location = new System.Drawing.Point(67, 344);
+            this.txt_ContraseñaLogin.Name = "txt_ContraseñaLogin";
+            this.txt_ContraseñaLogin.Size = new System.Drawing.Size(373, 22);
+            this.txt_ContraseñaLogin.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(67, 232);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 23);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Usuario";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(67, 306);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(125, 23);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Contraseña";
+            // 
+            // btn_Ingresar
+            // 
+            this.btn_Ingresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Ingresar.Location = new System.Drawing.Point(194, 390);
+            this.btn_Ingresar.Name = "btn_Ingresar";
+            this.btn_Ingresar.Size = new System.Drawing.Size(119, 48);
+            this.btn_Ingresar.TabIndex = 11;
+            this.btn_Ingresar.Text = "Ingresar";
+            this.btn_Ingresar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(905, 466);
+            this.Controls.Add(this.btn_Ingresar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_ContraseñaLogin);
+            this.Controls.Add(this.txt_UsuarioLogin);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pnl_imagen);
+            this.Controls.Add(this.pnl_titu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl_titu;
+        private System.Windows.Forms.Panel pnl_imagen;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.TextBox txt_UsuarioLogin;
+        private System.Windows.Forms.TextBox txt_ContraseñaLogin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_Ingresar;
+    }
+}
+
---
+++ UsuarioLogin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoIntegrador
+{
+    internal class UsuarioLogin
+    {
+
+        private static string _idUsu;
+        private static string usuario;
+        private static string nombres;
+        private static string apellidos;
+        private static string idRol;
+        private static string rol;
+        private static string foto;
+
+        public static string IdUsu {  get => _idUsu; set => _idUsu = value; }
+        public static string Usuario { get => usuario; set => usuario = value; }
+        public static string Nombres { get => nombres; set => nombres = value; }
+        public static string Apellidos { get => apellidos; set => apellidos = value; }
+        public static string IdRol { get => idRol; set => idRol = value; }
+        public static string Rol { get => rol; set => rol = value; }
+        public static string Foto { get => foto; set => foto = value; }
+
+
+
+
+    }
+}
